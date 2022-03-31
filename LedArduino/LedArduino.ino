92cf726feba15837bb758a12633678f5b95f8a88
Jaime Peraza <jaime.peraza@correo.usa.edu.co>
2022-03-31 12:51:14 -0500

    Se agrega función presentación

--- LedArduino/LedArduino.ino
+++ LedArduino/LedArduino.ino
@@ -1,14 +1,20 @@
 void Imprimir(int Value);
+void Presentacion();
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  Presentacion();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Imprimir(Value);
 }
-
+void Presentacion(){
+  Serial.print("BIENVENIDO USUARIO\n\n");
+  Serial.print("Este programa sensa la luz y a partir de esta activa un actuador\n");
+  Serial.print("Los integrantes del grupo son:\n");
+  Serial.print("Vanessa Cardenas    Jaime Peraza   Alejandro Suaterna   Mateo Torres\n");
+} 
 void Imprimir(int Value){
   Serial.print("El sensor de luz esta: ");
   if(Value>470 && Value<600){
@@ -18,5 +24,5 @@ void Imprimir(int Value){
     Serial.print("Apagado");
   }
   Serial.print("\n");
-  Delay(100);
+  delay(100);
 }
