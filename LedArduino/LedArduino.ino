36b680ecab844d1e7447339a6eb1341b74e1504f
alejosuaterna <pablo.gonzalez01@correo.usa.edu.co>
2022-03-31 16:33:19 -0500

    unificación de sensado y lighting con la interface

--- LedArduino/LedArduino.ino
+++ LedArduino/LedArduino.ino
@@ -1,13 +1,29 @@
+    int LDR = 0; //Pin analógico 4 para la foto resistencia
+    int LED = 2; //Pin digital para LED
+    int valor; //Acumula el valor del LDR
+
+    int max = 600; //Valor máximo de la fotoresistencia con luz plena
+    int min = 470; //Valor mínimo de la foto resuistencia sin luz
+    
 void Imprimir(int Value);
 void Presentacion();
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(9600); //Inicializa la comunicacion en serie
+  pinMode(LED, OUTPUT); //pin digital como salida
   Presentacion();
 }
 
 void loop() {
-  Imprimir(Value);
+  valor = analogRead(LDR); //Lee el valor del LDR
+  valor = constrain(valor, min, max); // Limita el valor entre el máximo y el mínimo
+  Imprimir(valor);
+  // Mostrar en monitor serial el valor para ajustar máx y min.
+  // Acomodar valor proporcional entre 0 y 255
+  // Cuando hay plena luz (máximo) --> Valor = 0
+  // Cuando no hay luz (mínimo) --> Valor = 255
+  valor = map(valor, max, min, 0, 255);
+  analogWrite(LED, valor); // Se enciende LED segun el valor obtenido
 }
 void Presentacion(){
   Serial.print("BIENVENIDO USUARIO\n\n");
