5b2eb2cab83ceaf8af1392f2daaf0536e7291474
Vanessa Cardenas <vanessacardenasmelendez@hotmail.com>
2022-03-31 16:13:36 -0500

    Se mejoró el codigo

--- prueba_Ardu/prueba_Ardu.ino
+++ prueba_Ardu/prueba_Ardu.ino
@@ -19,29 +19,5 @@ void loop() {
        // Cuando no hay luz (mínimo) --> Valor = 255
        valor = map(valor, max, min, 0, 255);
        analogWrite(LED, valor); // Se enciende LED segun el valor obtenido
-       Serial.println(LED, valor);
       
 }
-
-/*
-const int LEDPin = 2;
-const int LDRPin = A0;
-const int threshold = 100;
-
-void setup() {
- 
-  pinMode(LEDPin, OUTPUT);
-  pinMode(LDRPin, INPUT); // mín = 470 - Max = 600.
-}
-
-void loop() {
- 
-   int input = analogRead(LDRPin);
-   if (input > threshold) {
-      digitalWrite(LEDPin, HIGH);
-   }
-   else {
-      digitalWrite(LEDPin, LOW);
-   }
-}
-*/
