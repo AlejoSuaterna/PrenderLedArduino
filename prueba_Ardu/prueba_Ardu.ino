0257a6b920fe1f6f4a8c2a393df8cd96a979fd9b
Vanessa Cardenas <vanessacardenasmelendez@hotmail.com>
2022-03-31 13:34:34 -0500

    Por fin funcionó

--- prueba_Ardu/prueba_Ardu.ino
+++ prueba_Ardu/prueba_Ardu.ino
@@ -1,4 +1,4 @@
-    int LDR = 4; //Pin analógico 4 para la foto resistencia
+    int LDR = 0; //Pin analógico 4 para la foto resistencia
     int LED = 2; //Pin digital para LED
     int valor; //Acumula el valor del LDR
 
@@ -7,7 +7,7 @@
 
 void setup() {
     Serial.begin(9600); //Inicializa la comunicacion en serie
-    pinMode(LED, OUTPUT); //Digital pin como salida
+    pinMode(LED, OUTPUT); //pin digital como salida
 }
 
 void loop() {
@@ -19,4 +19,29 @@ void loop() {
        // Cuando no hay luz (mínimo) --> Valor = 255
        valor = map(valor, max, min, 0, 255);
        analogWrite(LED, valor); // Se enciende LED segun el valor obtenido
+       Serial.println(LED, valor);
+      
 }
+
+/*
+const int LEDPin = 2;
+const int LDRPin = A0;
+const int threshold = 100;
+
+void setup() {
+ 
+  pinMode(LEDPin, OUTPUT);
+  pinMode(LDRPin, INPUT); // mín = 470 - Max = 600.
+}
+
+void loop() {
+ 
+   int input = analogRead(LDRPin);
+   if (input > threshold) {
+      digitalWrite(LEDPin, HIGH);
+   }
+   else {
+      digitalWrite(LEDPin, LOW);
+   }
+}
+*/
